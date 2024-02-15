39e76d4b06e06229a746c6654c61b2623df5c1e1
AlbertEspanyol <albertespanyol12@gmail.com>
2024-02-15 14:02:34 +0100

    + WIP footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Header from './components/business/Header/Header.tsx';
 import Main from './components/pages/Main/Main.tsx';
+import Footer from './components/pages/Footer/Footer.tsx';
 import SplashScreen from './components/pages/SplashScreen/SplashScreen.tsx';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <SplashScreen />
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
